bc13c08bf609e6517488ab2a2c3047e004a67a01
Snehlata Mishra <snehlatamsr12@gmail.com>
2020-04-29 17:33:37 +0530

    Create DoubleEqualTripleEqual.js
    Double equal to and triple equal to example

---
+++ DoubleEqualTripleEqual.js
@@ -0,0 +1,14 @@
+var a=5
+var b=5
+if(a==b){
+	console.log(true);
+}else{
+	console.log(false);
+}
+
+var c="5" 
+if(a===c){
+	console.log(true);
+}else{
+	console.log(false); //comparing type and content. c is String and a is integer
+}
